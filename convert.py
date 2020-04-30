d1d118c973ad57609811dbfed3ff8559e47d648b
459488 <55198139+459488@users.noreply.github.com>
2020-04-29 22:28:33 -0400

    small tweaks

--- convert.py
+++ convert.py
@@ -1,5 +1,4 @@
 # convert.py - file io application to read in latest covid19 data and spit out Leaflet.js circles using string concatenation
-# March 24, 2020: mhoel - original coding 
 
 # Access data from: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
 # Korea, South - Bahamas, The - Gambia, The : Must be manually fixed in the data (South Korea, Bahamas, Gambia) 
@@ -17,6 +16,7 @@ count = 0 # count number of circles
 
 # loop through all rows in the csv file
 for line in datarows:
+	
 	templist = line.split(",")
 	province_state = templist[0]
 	country_region = templist[1]
@@ -27,8 +27,6 @@ for line in datarows:
 	recovered = templist[6]
 	active = templist[7]
 	last_update = templist[8]
-
-	# make radius of circle bigger for cartographic appeal
 	
 	if (int(confirmed) > 0):
 		if (province_state != ""):
